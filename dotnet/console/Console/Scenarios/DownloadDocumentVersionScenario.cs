15df3304b118b8e1a6ac3bcbaeaab809ebfedb4f
Jean Magalhães <jeanm@lacunasoftware.com>
2021-01-21 16:20:20 -0300

    Changed the method example

--- dotnet/console/Console/Scenarios/DownloadDocumentVersionScenario.cs
+++ dotnet/console/Console/Scenarios/DownloadDocumentVersionScenario.cs
@@ -19,13 +19,14 @@ namespace Console.Scenarios
             // 1. Get a document Id
             var result = await CreateDocumentAsync();
 
-            // 2. You can directly get an  especific document ticket 
+            // 2. You can directly get an  especific document ticket
+	        // You can get a ticket to a specific version of the document. The ticket is a temporary URL that allows you to download that version. 
             var ticketDownload = await SignerClient.GetDocumentDownloadTicketAsync(result.DocumentId, DocumentTicketType.Original);
 
             // 3. Get the document by passing it's Id and the Ticket type (This is a direct download and It already calls the method above)
             // Be sure to select the exact DocumentTicketType to download the type of document you want. 
             // Chek the available types by ispecting DocumentTicketType's ENUM.
-            var documentVersion = await SignerClient.GetDocumentAsync(result.DocumentId, DocumentTicketType.Original);
+            var documentVersion = await SignerClient.GetDocumentContentAsync(result.DocumentId, DocumentDownloadTypes.Original);
 
             // 4. You can also download a specific version type of the document encoded in Base 64 format.
             var documentVersionBytes = await SignerClient.GetDocumentBytesAsync(result.DocumentId, DocumentTicketType.Signatures);
@@ -33,11 +34,6 @@ namespace Console.Scenarios
 
             this.SaveFileStream("downloadversionExample.pdf", documentVersion);
 
-            /**
-             * NOTE: 
-             * There is deprecated method called GetDocumentContentAsync() that has the same return as the GetDocumentAsync() method.
-             * We recomend the use of the method applied at the example above.
-             */
 
         }
 
