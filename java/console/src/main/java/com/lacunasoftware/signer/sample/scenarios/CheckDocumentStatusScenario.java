b58a5a0b241cc193cc8ba88c8c74e4fc62c14ce8
Jean Magalhães <jeanm@lacunasoftware.com>
2021-02-17 15:19:42 -0300

    Created Webhook handle to deal with approved, signed and refused docs for Java

--- java/console/src/main/java/com/lacunasoftware/signer/sample/scenarios/CheckDocumentStatusScenario.java
+++ java/console/src/main/java/com/lacunasoftware/signer/sample/scenarios/CheckDocumentStatusScenario.java
@@ -1,6 +1,8 @@
 package com.lacunasoftware.signer.sample.scenarios;
 
 import java.io.IOException;
+
+import com.google.gson.Gson;
 import com.lacunasoftware.signer.ActionStatus;
 import com.lacunasoftware.signer.WebhookTypes;
 import com.lacunasoftware.signer.documents.CreateDocumentResult;
@@ -9,10 +11,9 @@ import com.lacunasoftware.signer.documents.DocumentModel;
 import com.lacunasoftware.signer.flowactions.FlowActionModel;
 
 import com.lacunasoftware.signer.javaclient.exceptions.RestException;
-import com.lacunasoftware.signer.webhooks.DocumentConcludedModel;
-import com.lacunasoftware.signer.webhooks.WebhookModel;
+import com.lacunasoftware.signer.webhooks.*;
 
-public class CheckDocumentStatusScenario extends Scenario implements IWebhookHandlerScenario {
+public class CheckDocumentStatusScenario extends Scenario implements IWebhookHandlerScenario  {
     /**
      * This scenario demonstrates how to check if a document is concluded and the
      * status of it's flow actions.
@@ -23,7 +24,7 @@ public class CheckDocumentStatusScenario extends Scenario implements IWebhookHan
 
         // 1. Get the document's details by it's id
         DocumentModel details = signerClient.getDocumentDetails(result.getDocumentId());
-   
+
         // 2. Check if the whole flow is concluded
         if (details.isIsConcluded()) {
 
@@ -51,13 +52,30 @@ public class CheckDocumentStatusScenario extends Scenario implements IWebhookHan
          */
     }
 
-	@Override
+    @Override
 	public void HandleWebhook(WebhookModel webhook) {
-		if (webhook != null) {
-            if (webhook.getType() == WebhookTypes.DOCUMENTCONCLUDED) {
-                DocumentConcludedModel documentConcludedModel = (DocumentConcludedModel)webhook.getData();
+        //Be aware when deserializing OffsetDateOfTime using another Json library, you must pay attention to it's data type
+        //We have a specific method to deserialize OffsetDateOfTime fields using signerClient:
+        Gson gson = signerClient.getGson();
 
+        if (webhook != null) {
+            // We parse webhook.data to json again to deserialize it properly
+           String webhookData = gson.toJson(webhook.getData());
+            if (webhook.getType() == WebhookTypes.DOCUMENTCONCLUDED) {
+                DocumentConcludedModel documentConcludedModel = gson.fromJson(webhookData, DocumentConcludedModel.class);
                 System.out.println(String.format("Document %s is concluded", documentConcludedModel.getId()));
+            }
+            if (webhook.getType() == WebhookTypes.DOCUMENTREFUSED) {
+                DocumentRefusedModel documentRefusedModel = gson.fromJson(webhookData, DocumentRefusedModel.class);
+                System.out.println(String.format("Document %s is Refused", documentRefusedModel.getId()));
+            }
+            if (webhook.getType() == WebhookTypes.DOCUMENTAPPROVED) {
+                DocumentApprovedModel documentApprovedModel = gson.fromJson(webhookData, DocumentApprovedModel.class);
+                System.out.println(String.format("Document %s is Approved", documentApprovedModel.getId()));
+            }
+            if (webhook.getType() == WebhookTypes.DOCUMENTSIGNED) {
+                DocumentSignedModel documentSignedModel = gson.fromJson(webhookData, DocumentSignedModel.class);
+                System.out.println(String.format("Document %s is Signed", documentSignedModel.getId()));
             }
         }
 	}
