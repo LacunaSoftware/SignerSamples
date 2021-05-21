687b93b604d4e45ffda0bcc8d463bae9e73c9239
Jean Magalhães <jeanm@lacunasoftware.com>
2021-05-20 22:07:21 -0300

    Fixed comments php example

--- php/Scenarios/CreateDocumentWithAttachmentScenario.php
+++ php/Scenarios/CreateDocumentWithAttachmentScenario.php
@@ -37,27 +37,27 @@ class CreateDocumentWithAttachmentScenario extends Scenario
         $attachment = fopen($attachmentPath, "r");
         $attachmentUpload = new UploadModel($this->signerClient->uploadFile($attachmentName, $attachment, "application/pdf"));
 
-        // 2. Define the name of the document which will be visible in the application
+        // 3. Define the name of the document which will be visible in the application
         $attachmentUploadModel = new AttachmentUploadBuilder($attachmentUpload);
         $attachmentUploadModel->setDisplayName("One Attachment Sample");
 
 
 
-        // 3. For each participant on the flow, create one instance of ParticipantUserModel
+        // 4. For each participant on the flow, create one instance of ParticipantUserModel
         $user = new UsersParticipantUserModel();
         $user->setName("Jack Bauer");
         $user->setEmail("jack.bauer@mailinator.com");
         $user->setIdentifier("05819884183");
 
 
-        // 4. Create a FlowActionCreateModel instance for each action (signature or approval) in the flow.
+        // 5. Create a FlowActionCreateModel instance for each action (signature or approval) in the flow.
         //    This object is responsible for defining the personal data of the participant and the type of
         //    action that he will perform on the flow
         $flowActionCreateModel = new FlowActionsFlowActionCreateModel();
         $flowActionCreateModel->setType(FlowActionType::APPROVER);
         $flowActionCreateModel->setUser($user);
 
-        // 5. Send the document create request with whith the attachment attribute
+        // 6. Send the document create request with whith the attachment attribute
         $documentRequest = new DocumentsCreateDocumentRequest();
         $documentRequest->setFiles(
             array($fileUploadModelBuilder->toModel())
@@ -69,7 +69,7 @@ class CreateDocumentWithAttachmentScenario extends Scenario
             array($flowActionCreateModel)
         );
 
-        // 6. Result
+        // 7. Result
         $docResult = new DocumentsCreateDocumentResult($this->signerClient->createDocument($documentRequest));
 
         echo "Document " . $docResult->getDocumentId() . " created\n";
