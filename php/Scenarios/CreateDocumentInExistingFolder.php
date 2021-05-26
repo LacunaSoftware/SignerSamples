f39be03056e48c33947f180321a2a820ea89774d
jrodriguesmagal <jrodriguesmagal@gmail.com>
2021-05-25 21:47:37 -0300

    Added  CreateDocumentInExistingFolder scenario

---
+++ php/Scenarios/CreateDocumentInExistingFolder.php
@@ -0,0 +1,73 @@
+<?php
+
+
+namespace Lacuna\Scenarios;
+
+
+use Lacuna\Signer\Model\DocumentsCreateDocumentRequest;
+use Lacuna\Signer\Model\DocumentsCreateDocumentResult;
+use Lacuna\Signer\Model\FlowActionsFlowActionCreateModel;
+use Lacuna\Signer\Model\FlowActionType;
+use Lacuna\Signer\Model\FoldersFolderInfoModel;
+use Lacuna\Signer\Model\UsersParticipantUserModel;
+use Lacuna\Signer\PhpClient\Builders\FileUploadBuilder;
+use Lacuna\Signer\PhpClient\Models\UploadModel;
+use Lacuna\Signer\PhpClient\Params\PaginatedSearchParams;
+
+class CreateDocumentInExistingFolder extends Scenario
+{
+    /**
+     * This scenario demonstrates the creation of a document into an existing
+     * folder.
+     */
+    function run()
+    {
+        // 1. The file's bytes must be read by the application and uploaded
+        $filePath = "sample.pdf";
+        $fileName = basename($filePath);
+        $file = fopen($filePath, "r");
+        $uploadModel = new UploadModel($this->signerClient->uploadFile($fileName, $file, "application/pdf"));
+
+        // 2. Define the name of the document which will be visible in the application
+        $fileUploadModelBuilder = new FileUploadBuilder($uploadModel);
+        $fileUploadModelBuilder->setDisplayName("One Signer Sample");
+
+        // 3. For each participant on the flow, create one instance of ParticipantUserModel
+        $user = new UsersParticipantUserModel();
+        $user->setName("Jack Bauer");
+        $user->setEmail("jack.bauer@mailinator.com");
+        $user->setIdentifier("75502846369");
+
+
+        // 4. Create a FlowActionCreateModel instance for each action (signature or approval) in the flow.
+        //    This object is responsible for defining the personal data of the participant and the type of
+        //    action that he will perform on the flow
+        $flowActionCreateModel = new FlowActionsFlowActionCreateModel();
+        $flowActionCreateModel->setType(FlowActionType::SIGNER);
+        $flowActionCreateModel->setUser($user);
+
+
+        $paginatedSearchParams = new PaginatedSearchParams();
+        $paginatedSearchParams->setQ("New Folder");
+
+        $folders = $this->signerClient->listFoldersPaginated($paginatedSearchParams, null);
+        $folder = $folders->getItems()[0];
+
+
+        // 5. Send the document create request
+        $documentRequest = new DocumentsCreateDocumentRequest();
+
+        $documentRequest->setFolderId($folder->getId());
+        $documentRequest->setFiles(
+            array($fileUploadModelBuilder->toModel())
+        );
+        $documentRequest->setFlowActions(
+            array($flowActionCreateModel)
+        );
+
+        $docResult = $this->signerClient->createDocument($documentRequest)[0];
+
+        echo "Document " . $docResult->getDocumentId() . " created\n";
+
+    }
+}
