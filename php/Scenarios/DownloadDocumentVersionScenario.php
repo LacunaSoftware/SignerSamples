02684b61eff5a43dde56eaa8eb1c472d64148f55
jrodriguesmagal <jrodriguesmagal@gmail.com>
2021-06-01 19:44:06 -0300

    Added comment

--- php/Scenarios/DownloadDocumentVersionScenario.php
+++ php/Scenarios/DownloadDocumentVersionScenario.php
@@ -11,6 +11,9 @@ use Lacuna\Signer\Model\DocumentTicketType;
 
 class DownloadDocumentVersionScenario extends Scenario
 {
+    /**
+     * This scenario describes how to download documents.
+     */
     function run()
     {
         // 1. Get a document Id
@@ -23,12 +26,10 @@ class DownloadDocumentVersionScenario extends Scenario
         // Be sure to select the exact DocumentTicketType to download the type of document you want.
         // Check the available types by inspecting DocumentTicketType's ENUM.
         $documentVersion = $this->signerClient->getDocumentContent($result->getDocumentId(), DocumentDownloadTypes::ORIGINAL);
-
         file_put_contents("resources/downloads/documentVersion.pdf", $documentVersion);
 
         // 4. You can also get the bytes directly instead of a Stream for a specific version type of the document
         $documentVersionBytes = new DocumentsDocumentContentModel($this->signerClient->GetDocumentContentBytesAsync($result->getDocumentId(), DocumentDownloadTypes::ORIGINAL));
-
         file_put_contents("resources/downloads/documentVersionBytes.pdf", base64_decode($documentVersionBytes["bytes"]));
 
     }
